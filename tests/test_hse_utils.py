60673510d3ae48860164c9347d5d367f6ac606ab
nanthan06-cloud <nanthan06@gmail.com>
2026-09-16 15:52:06 +0800

    Add missing test for incident_rate with multiple incidents

--- tests/test_hse_utils.py
+++ tests/test_hse_utils.py
@@ -7,3 +7,7 @@ def test_incident_rate_basic():
 
 def test_incident_rate_zero_hours():
     assert incident_rate(5, 0) == 0.0
+
+
+def test_incident_rate_multiple_incidents():
+    assert incident_rate(3, 100_000) == 6.0
